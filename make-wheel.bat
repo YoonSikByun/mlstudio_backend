87da0d5a1daae3dbabb29586c46e60e632212909
YS <yunsikpyun@gmail.com>
2024-06-20 23:12:59 +0900

    modify

--- make-wheel.bat
+++
@@ -1,2 +0,0 @@
-python setup.py bdist_wheel && cd dist && twine upload -r nexus mlstudio_backend-0.1-py3-none-any.whl
-pause
---
+++ tests/test.py
@@ -0,0 +1,8 @@
+import duckdb
+import numpy as np
+
+test = {'a' : 1, 'b' : 2}
+
+print(test)
+
+print(dict(test, sor_dd='sdfadfasdf'))
